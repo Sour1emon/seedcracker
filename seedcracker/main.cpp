a3071bea1344d47fd40cbcaf0363e5f75db9426a
Sour1emon <isaac.bess@outlook.com>
2024-12-23 09:23:16 -0800

    Initial Commit

---
+++ seedcracker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  seedcracker
+//
+//  Created by Isaac Bess on 12/23/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
